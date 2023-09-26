d9c439b4078993ad8e5e821c500fdc689f1de969
Kirmct de Abreu <kirmctneto@gmail.com>
2023-09-26 09:34:53 -0400

    validacoes e dto

---
+++ Api01.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api01.Application.DTOs.Validations;
+
+public class PersonDTOValidator : AbstractValidator<PersonDTO>
+{
+	public PersonDTOValidator()
+	{
+		RuleFor(x => x.Documento)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Document deve ser informado!");
+
+		RuleFor(x => x.Name)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Name deve ser informado!");
+
+		RuleFor(x => x.Phone)
+			.NotEmpty()
+			.NotNull()
+			.WithMessage("Phone deve ser informado!");
+	}
+}
